3dbd8cbbdeb9a6909501c252012e9eaf31fe4a57
Ali Bayat Mokhtari <ali.bayatmokhtari96@gmail.com>
2024-08-15 15:22:17 +0330

    feat: add cass baseline

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Home } from "./ui/views";
 import { AppHeader, Rtl, Theme } from "./ui/components";
+import CssBaseline from "@mui/material/CssBaseline";
 import { theme } from "./ui/theme";
 
 function Root() {
 	return (
 		<Rtl>
 			<Theme theme={theme}>
+				<CssBaseline />
 				<AppHeader />
 				<Home />
 			</Theme>
