e451cde9d2bc205a689e00666a2459b45f0682e0
Ali Bayat Mokhtari <ali.bayatmokhtari96@gmail.com>
2024-09-22 14:39:12 +0330

    feat: app header

--- src/ui/components/app-header/AppHeader.tsx
+++ src/ui/components/app-header/AppHeader.tsx
@@ -1,37 +1,50 @@
 import { fa } from "../../i18n";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
+import styled from "@emotion/styled";
+
+import logo from "@/assets/logo.png";
+import { PropsWithChildren } from "react";
+
+const Logo = styled.img`
+	width: 64px;
+	height: 64px;
+`;
+
+function AuthButton({ children }: PropsWithChildren) {
+	return (
+		<Button
+			variant="text"
+			color="inherit"
+		>
+			{children}
+		</Button>
+	);
+}
 
 export function AppHeader() {
 	return (
 		<AppBar
 			position="sticky"
 			elevation={0}
+			color="default"
 		>
 			<Container>
-				<Toolbar>
-					<IconButton
-						size="large"
-						edge="start"
-						color="inherit"
-						aria-label="menu"
-						sx={{ mr: 2 }}
-					>
-						<MenuIcon />
-					</IconButton>
+				<Toolbar sx={{ py: 2 }}>
+					<Logo src={logo} />
 					<Typography
-						variant="h6"
-						component="div"
+						component="h1"
 						sx={{ flexGrow: 1 }}
+						ml={2}
+						fontSize={28}
 					>
 						{fa.app_header.title}
 					</Typography>
-					<Button>{fa.app_header.login}</Button>
+					<AuthButton>{fa.app_header.register}</AuthButton>
+					<AuthButton>{fa.app_header.login}</AuthButton>
 				</Toolbar>
 			</Container>
 		</AppBar>
